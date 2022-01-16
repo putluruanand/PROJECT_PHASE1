b9d423844d9a27811e53d645efbd7dc23bc72f2b
putluruanand <97275166+putluruanand@users.noreply.github.com>
2022-01-16 11:20:14 +0530

    Add files via upload

---
+++ LockedMeMain.java
@@ -0,0 +1,14 @@
+package com.anand;
+
+
+public class LockedMeMain {
+public static void main(String[] args) {
+		
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+		
+		menuOptions.printWelcomeScreen("Locker", "anand");
+		
+		HandleOption.handleWelcomeScreenInput();
+	}
+}
